de8fff46d9dafb9af6b66a4f9fd1404a13fb1af4
Fabio Coelho <fbclh@protonmail.com>
2022-04-25 16:10:07 -0300

    Update handlers to be able to change branch

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,41 +1,84 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class InputTodo extends Component {
-  handleSubmit = (event) => {
+export const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
+
+  const handleChange = (event) => {
+    setInputText({
+      ...inputText,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title);
-      this.setState({ title: '' }); // reset the input field
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title);
+      setInputText({
+        title: '',
+      }); // reset the input field
     } else {
       alert('Please enter a todo');
     }
   };
 
-  state = {
-    title: '',
-  };
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <input
+        className="input-text"
+        type="text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={handleChange}
+      />
+      <button className="input-submit" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+//* class based component
+// import { Component } from 'react';
+// export class InputTodo extends Component {
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+//     if (this.state.title.trim()) {
+//       this.props.addTodoProps(this.state.title);
+//       this.setState({ title: '' }); // reset the input field
+//     } else {
+//       alert('Please enter a todo');
+//     }
+//   };
 
-  render() {
-    return (
-      <form className="form-container" onSubmit={this.handleSubmit}>
-        <input
-          className="input-text"
-          type="text"
-          placeholder="Add todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.handleChange}
-        />
-        <button className="input-submit" type="submit">
-          Add
-        </button>
-      </form>
-    );
-  }
-}
+//   state = {
+//     title: '',
+//   };
+
+//   handleChange = (event) => {
+//     this.setState({
+//       [event.target.name]: event.target.value,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <form className="form-container" onSubmit={this.handleSubmit}>
+//         <input
+//           className="input-text"
+//           type="text"
+//           placeholder="Add todo..."
+//           value={this.state.title}
+//           name="title"
+//           onChange={this.handleChange}
+//         />
+//         <button className="input-submit" type="submit">
+//           Add
+//         </button>
+//       </form>
+//     );
+//   }
+// }
